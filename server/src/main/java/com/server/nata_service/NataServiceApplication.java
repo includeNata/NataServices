1c53d5d980589357c5d7f0f05ea9f984400af762
Alerrando <alerrando2@gmail.com>
2024-07-07 20:16:07 -0300

    feat: add server

---
+++ server/src/main/java/com/server/nata_service/NataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.server.nata_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NataServiceApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/server/nata_service/NataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.nata_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
