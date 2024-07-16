a7726ec7e098973eed6f9a6968f6dad5e78098b4
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2024-07-16 16:12:13 -0300

    feat: Add 201 Created response and remove duplicate if condition in SkillController
    -Modified createSkill method to return 201 Created status with URI of the new resource.
    -Added id field to SkillDTO for URI creation.
    -Removed duplicate if condition to improve code clarity.

--- server/src/main/java/com/server/nata_service/controllers/SkillController.java
+++ server/src/main/java/com/server/nata_service/controllers/SkillController.java
@@ -1,5 +1,6 @@
 package com.server.nata_service.controllers;
 
+import com.server.nata_service.dto.EmployeeDTO;
 import com.server.nata_service.dto.SkillDTO;
 import com.server.nata_service.services.SkillService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/skill")
@@ -24,20 +28,20 @@ public class SkillController {
     public ResponseEntity<SkillDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(skillService.findById(id));
     }
+
     @PostMapping
     public ResponseEntity<SkillDTO> createSkill(@RequestBody SkillDTO skillDTO) {
         SkillDTO createdSkill = skillService.createSkill(skillDTO);
-        return ResponseEntity.ok(createdSkill);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdSkill.getId()).toUri();
+        System.out.println(ResponseEntity.created(uri).body(createdSkill));
+        return ResponseEntity.created(uri).body(createdSkill);
     }
+
 
     @PutMapping("/{id}")
     public ResponseEntity<SkillDTO> updateSkill(@PathVariable Long id, @RequestBody SkillDTO skillDTO) {
         SkillDTO updatedSkill = skillService.updateSkill(id, skillDTO);
-        if (updatedSkill != null) {
-            return ResponseEntity.ok(updatedSkill);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.ok(updatedSkill);
     }
     
 }
