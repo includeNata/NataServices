ea456f2bb4cdab90c4a52849aa33c84ec7f40cbb
Leo Rong <112832113+LeoRong1@users.noreply.github.com>
2024-07-18 23:18:04 -0300

    add- entities

---
+++ server/src/main/java/com/server/nata_service/entities/Area.java
@@ -0,0 +1,63 @@
+package com.server.nata_service.entities;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "area")
+public class Area {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "varchar(50)")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_area_id", nullable = false)
+    private Employee employee;
+    public Area() {}
+
+    public Area(Long id, String name, Employee employee) {
+        this.id = id;
+        this.name = name;
+        this.employee = employee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Area area = (Area) o;
+        return Objects.equals(id, area.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ server/src/main/java/com/server/nata_service/entities/Certificate.java
@@ -0,0 +1,64 @@
+package com.server.nata_service.entities;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "certificate")
+public class Certificate {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "varchar(50)")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_certificate_id", nullable = false)
+    private Employee employee;
+
+    public Certificate() {}
+
+    public Certificate(Long id, String name, Employee employee) {
+        this.id = id;
+        this.name = name;
+        this.employee = employee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Certificate certificate = (Certificate) o;
+        return Objects.equals(id, certificate.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
